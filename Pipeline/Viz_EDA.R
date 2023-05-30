a6f2f0440a0dc1cf46fe01721d77e173adeebcd3
https://ucsd.bootcampcontent.com/UCSD-Coding-Bootcamp/UCSDSDG201907DATA2.git <ruddy.simonpour@gmail.com>
2023-05-30 12:45:28 -0700

    checking missing data

--- Pipeline/Viz_EDA.R
+++ Pipeline/Viz_EDA.R
@@ -2,7 +2,6 @@
 plot_distributions <- function(df, column_indices) {
   selected_cols <- names(df)[column_indices]
   
-  # Analyzing the distribution of variables
   for(col in selected_cols){
     p <- ggplot(df, aes_string(col)) + geom_histogram(bins = 30) + theme_minimal() + 
         labs(title=col, x=NULL) +  theme(plot.title = element_text(hjust = 0.5))
